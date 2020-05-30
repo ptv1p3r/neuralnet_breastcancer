f4626c36f0839f849a999fb4b34f2db6a222ded5
Márcio <marciomds94@hotmail.com>
2020-05-30 18:07:19 +0100

    add some features

--- neuralnet_test.py
+++ neuralnet_test.py
@@ -1,10 +1,22 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+
 import os
 import os.path
 from os import path
 
+from sklearn.preprocessing import LabelEncoder
+
+# Ver estatisticas
+import seaborn as sns
+
+# ################################## Develop Mode #######################################################################
+# Mostra no terminal o resultado de cada passo efetuado
+debug = True
+plot_graphics = False
 # ################################## Directorias #######################################################################
 # URL da BD
 data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data'
@@ -19,9 +31,8 @@ file_name = "wdbc.data"
 data_name = 'wdbc.data'
 
 # ########################### Opções aplicadas ao nosso modelo e tratamento de dados ###################################
-# Set to True to shuffle the data before you split into training and # test sets
-do_shuffle = True
-
+# Defenir se queremos baralhar os dados antes de fazer split para os dados de treino e dados de teste
+do_shuffle = False
 
 # Define corretamente os caminhos do dataset
 app_root = os.path.dirname(os.path.abspath(__file__))
@@ -38,11 +49,33 @@ if not path.exists(database_path) or not path.exists(database_path):
     # isto deixa a app mais dinamica para que no futuro possamos trabalhar outros dados
     file_name = tf.keras.utils.get_file(fname=database_path, origin=data_url)
 
+
 # Load dos dados da nossa DB
 my_data = pd.read_csv(database_path, delimiter=',')
 
-# Confirmar os dados
-print(my_data)
+if debug:
+    print("")
+    print("========== Dados row do dataset ==========")
+    print("")
+    print(my_data)
+    print("")
+
+my_data.columns = ['id', 'diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness',
+                   'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension', 'radius_se',
+                   'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se',
+                   'concave_points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worse', 'texture_worst',
+                   'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst',
+                   'concave_points_worst', 'symmetry_worst', 'fractal_dimension_worst']
+
+
+if debug:
+    # Confirmar as colunas se estão corretas
+    print("")
+    print("========== Dados row com nomes nas colunas ==========")
+    print("")
+    print(my_data)
+    print("")
+
 
 # Os dados podem ter algum tipo de ordenação e isso pode afetar o treino
 # Por exemplo se estiverem ordenados por tamanho e começar no Benigno para o Maligno
@@ -53,6 +86,67 @@ if do_shuffle:
 else:
     randomized_data = my_data
 
-# DEUBUG de confirmação
-print(randomized_data)
 
+if debug:
+    print("")
+    print("========== Shuffle dos dados ==========")
+    print("")
+    print(randomized_data)
+    print("")
+
+
+if debug:
+    print("")
+    print("========== Verificar se existem campos sem informação por coluna ==========")
+    print("")
+    print(randomized_data.isna().sum())
+    print("")
+
+
+if plot_graphics:
+    # Grafico do n° de cancros Benignos ou Malignos
+    # Isto é apresentado quando corrermos o comando plt.show()
+    sns.countplot(randomized_data['diagnosis'], label='count')
+
+
+# perceber o tipo de campos que temos no dataset
+if debug:
+    print("")
+    print("========== Tipo de Coluna do dataset ==========")
+    print("")
+    print(randomized_data.dtypes)
+    print("")
+
+
+# Index 1 é referente a coluna diagnosis, os ":" é referente a todas as linhas
+# Transformo os valored de M no valor 1 e B no valor 0
+# e volto a guardar a coluna
+labelencoder_Y = LabelEncoder()
+randomized_data.iloc[:, 1] = labelencoder_Y.fit_transform(randomized_data.iloc[:, 1].values)
+
+
+if debug:
+    print("")
+    print("========== Valores de diagnosis passados para binario ==========")
+    print("")
+    print(randomized_data.iloc[:, 1])
+    print("")
+
+
+if plot_graphics:
+    # Cria um par plot da coluna 1 a 6 sabendo que começa no 0
+    # Aplicando o K-means apenas para visualizar e não para ordenar
+    # Apenas para termos uma visualização dos clusters
+    # Azul é o valor 0(B)
+    # Laranja é o valor 1(M)
+    sns.pairplot(randomized_data.iloc[:, 1:11], hue='diagnosis')
+    plt.show()
+
+
+if debug:
+    # Mostra quais colunas interferem com quais colunas
+    print("")
+    print("========== Correlação entre colunas ==========")
+    print("")
+    print(randomized_data.iloc[:, 1:12].corr())
+    print("")
