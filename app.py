17301d196678d9e5ab3e0703d524ba8ca556b87a
Márcio <marciomds94@hotmail.com>
2020-06-01 14:08:46 +0100

    several fixes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from config import *
 import tensorflow as tf
+
 from dataset_organization import dataset
 from models import model_decision_tree_classifier, model_logistic_regression, model_random_forest_classifier, model_sequential
 
@@ -14,6 +15,21 @@ print('#########################################################################
 classMmodel = model_random_forest_classifier(X_train, Y_train)
 print('###############################################################################################################')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('---------------------------------------------------------------------------------------------------------------')
 modelSequential, history_dict = model_sequential(X_train, Y_train)
 
 # Avaliação do Modelo Sequencial
@@ -26,12 +42,25 @@ print("Test accuracy: ", acc)
 modelSequential.save(models_path)
 
 # Apaga o modelo anterior para testar
-del modelSequential
+# del modelSequential
+#
+# modelTest = tf.keras.models.load_model(models_path)
 
-modelTest = tf.keras.models.load_model(models_path)
-
-loss, acc = modelTest.evaluate(X_test, Y_test, verbose=2)
-print("Test loss: ", loss)
-print("Test accuracy: ", acc)
+# loss, acc = modelTest.evaluate(X_test, Y_test, verbose=2)
+# print("Test loss: ", loss)
+# print("Test accuracy: ", acc)
 
 
+print('---------------------------------------------------------------------------------------------------------------')
+
+# Teste da avaliação e do predict do modelo sequencial
+# Evaluate the model on the test data using `evaluate`
+print('\n# Evaluate on test data')
+results = modelSequential.evaluate(X_test, Y_test, batch_size=128)
+print('test loss, test acc:', results)
+
+# Generate predictions (probabilities -- the output of the last layer)
+# on new data using `predict`
+print('\n# Generate predictions for 3 samples')
+predictions = modelSequential.predict(X_test[:3])
+print('predictions shape:', predictions.shape)
--- models.py
+++ models.py
@@ -10,26 +10,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 
-#
-# def models(x_train, y_train):
-#     # Logistic Regression
-#     log = LogisticRegression(random_state=0)
-#     log.fit(x_train, y_train)
-#
-#     # Decision Tree
-#     tree = DecisionTreeClassifier(criterion='entropy', random_state=0)
-#     tree.fit(x_train, y_train)
-#
-#     # Random Forest Classifier
-#     forest = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
-#     forest.fit(x_train, y_train)
-#
-#     # Model Accuracy sobre os dados de treino
-#     print('[0] Logistic Regression Training Accuracy:', log.score(x_train, y_train))
-#     print('[1] Decision Tree Classifier Training Accuracy:', tree.score(x_train, y_train))
-#     print('[2] Random Forest Classifier Training Accuracy:', forest.score(x_train, y_train))
-#
-#     return log, tree, forest
 
 def model_logistic_regression(x_train, y_train):
     # Logistic Regression
